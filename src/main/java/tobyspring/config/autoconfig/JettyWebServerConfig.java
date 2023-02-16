9c2fb2af2e82a433003e0c15e2219bbebd59f0b8
choahra <ahar0729@gamil.com>
2023-02-16 22:49:46 +0900

    라이브러리에 서버가 포함되어있으면 true 반환하도록 로직 수정

--- src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
@@ -21,7 +22,8 @@ public class JettyWebServerConfig {
     static class JettyCondition implements Condition {
         @Override
         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
+            return ClassUtils.isPresent("org.eclipse.jetty.server.Server",
+                    context.getClassLoader()); //라이브러리에 org.eclipse.jetty.server.Server 포함이 되어있으면 true 반환
         }
     }
 }
--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,6 +1,7 @@
 package tobyspring.config.autoconfig;
 
 
+import com.fasterxml.jackson.databind.util.ClassUtil;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
@@ -21,7 +23,8 @@ public class TomcatWebServerConfig {
     static class TomcatCondition implements Condition {
         @Override
         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return true;
+            return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat",
+                    context.getClassLoader()); //라이브러리에 org.apache.catalina.startup.Tomcat 포함이 되어있으면 true 반환
         }
     }
 }
