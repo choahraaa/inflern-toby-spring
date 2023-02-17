75bda2a46176d07ddc023caaa141d7dde63c67f9
choahra <ahar0729@gamil.com>
2023-02-18 00:20:15 +0900

    application.properties 파일에 등록해 놓은 환경변수 가져오기

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,9 +1,21 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
-public class HellobootApplication {
+public class  HellobootApplication {
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment env) {
+        return args -> {
+            String name = env.getProperty("my.name");
+            System.out.println("my name " + name);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HellobootApplication.class, args);
