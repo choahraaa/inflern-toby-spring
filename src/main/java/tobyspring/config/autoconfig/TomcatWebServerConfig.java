540493dec149c7affa1ad0d8f10db84e638d43a2
choahra <ahar0729@gamil.com>
2023-02-18 21:53:38 +0900

    property 로직 분리 후 imports파일에 추가

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -13,17 +13,13 @@ import tobyspring.config.MyAutoConfiguration;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
-    @Value("${contextPath:}")
-    String contextPath;
-    @Value("${port:8080}") //지정한 프로퍼티 값이 없으면 default값으로 설정 (: 사용)
-    int port;
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean // 동일한 타입의 bean이 있는지 체크하고 없으면 사용하겠다는 의미(사용자 정보에 저장된 bean을 우선 처리)
-    public ServletWebServerFactory ServletWebServerFactory() {
+    public ServletWebServerFactory ServletWebServerFactory(ServerProperties properties) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
 
-        factory.setContextPath(this.contextPath);
-        factory.setPort(this.port);
+        factory.setContextPath(properties.getContextPath());
+        factory.setPort(properties.getPort());
 
         return factory;
     }
